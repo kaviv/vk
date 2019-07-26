1b8bf4b9cbf7f0e2e3ac8a266508ffb046c9135c
kaviv <53109444+kaviv@users.noreply.github.com>
2019-07-26 17:33:55 +0530

    Create vk.py

---
+++ vk.py
@@ -0,0 +1,6 @@
+chr= input()
+
+if((chr >= 'a' and chr <= 'z') or (chr >= 'A' and chr <= 'Z')):
+    print("The Given Character ", chr, "is an Alphabet")
+else:
+    print("The Given Character ", chr, "is Not an Alphabet")
